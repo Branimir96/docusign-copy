97417f9c4ff397bfd26c01141d652a64f3485c15
ena <ena.kozul@gmail.com>
2026-02-02 17:44:25 +0100

    Revert "feat: rebrand email templates from Documenso to DokuHR"
    This reverts commit 3e823f8f66a7b32e313161c3137e833dbfb651e8.

--- packages/email/template-components/template-footer.tsx
+++ packages/email/template-components/template-footer.tsx
@@ -17,7 +17,7 @@ export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
           <Trans>
             This document was sent using{' '}
             <Link className="text-[#7AC455]" href="https://documen.so/mail-footer">
-              DokuHR
+              Documenso
             </Link>
             .
           </Trans>
@@ -34,6 +34,14 @@ export const TemplateFooter = ({ isDocument = true }: TemplateFooterProps) => {
               </>
             );
           })}
+        </Text>
+      )}
+
+      {!branding.brandingEnabled && (
+        <Text className="my-8 text-sm text-slate-400">
+          Documenso, Inc.
+          <br />
+          2261 Market Street, #5211, San Francisco, CA 94114, USA
         </Text>
       )}
     </Section>
--- packages/email/templates/access-auth-2fa.tsx
+++ packages/email/templates/access-auth-2fa.tsx
@@ -45,7 +45,11 @@ export const AccessAuth2FAEmailTemplate = ({
               {branding.brandingEnabled && branding.brandingLogo ? (
                 <Img src={branding.brandingLogo} alt="Branding Logo" className="mb-4 h-6" />
               ) : (
-                <Img src={getAssetUrl('/static/logo.png')} alt="dokuHR Logo" className="mb-4 h-6" />
+                <Img
+                  src={getAssetUrl('/static/logo.png')}
+                  alt="Documenso Logo"
+                  className="mb-4 h-6"
+                />
               )}
 
               <TemplateAccessAuth2FA
